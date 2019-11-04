41a815d80d2f95d3d8dbda88d10875570826a610
Christopher Ray Langford <chris@hellocld.com>
2019-11-04 15:41:31 -0500

    Ball and paddle both use KinematicBody2D, ball bounces off paddle

--- Ball.gd
+++ Ball.gd
@@ -1,17 +1,14 @@
-extends Area2D
+extends KinematicBody2D
 
 export var _speed = 10
 var _dir = Vector2(1, 1)
 
-func _process(delta):
-	position += _dir * _speed * delta
+func _physics_process(delta):
+	var collision = move_and_collide(_dir * _speed * delta)
+	if collision:
+		print(collision.normal)
+		_dir = _dir.bounce(collision.normal)
 	if position.x >= get_viewport().size.x or position.x < 0:
 		_dir.x *= -1
 	if position.y >= get_viewport().size.y or position.y < 0:
 		_dir.y *= -1
-
-
-
-
-func _on_Ball_area_entered(area):
-	print("Area entered!")
