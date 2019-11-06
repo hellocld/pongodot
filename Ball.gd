1464c1ff5590329b597b9c5170b0431e3148aaf7
Christopher Ray Langford <chris@hellocld.com>
2019-11-05 20:37:08 -0500

    It's not very fun, but it's a game!
    Keeps tabs on the score and stops resetting the ball once someone wins. Outputs the score to stdout.

--- Ball.gd
+++ Ball.gd
@@ -3,12 +3,21 @@ extends KinematicBody2D
 export var _speed = 10
 var _dir = Vector2(1, 1)
 
+func _ready():
+	reset()
+
+
 func _physics_process(delta):
 	var collision = move_and_collide(_dir * _speed * delta)
 	if collision:
-		print(collision.normal)
+		print(collision.collider.name)
 		_dir = _dir.bounce(collision.normal)
 	if position.x >= get_viewport().size.x or position.x < 0:
 		_dir.x *= -1
 	if position.y >= get_viewport().size.y or position.y < 0:
 		_dir.y *= -1
+
+
+func reset():
+	_dir = _dir.normalized()
+	position = get_viewport().size / 2
--- Game.gd
+++ Game.gd
@@ -1,6 +1,35 @@
 extends Node2D
 
+export var game_score = 10
+
+var p1_score = 0
+var p2_score = 0
 
 
-func _on_Goal_body_entered(body):
-	print("Body " + body.name + " entered")
+func _ready():
+	p1_score = 0
+	p2_score = 0
+
+func _print_score():
+	print("P1 %d | P2 %d" % [p1_score, p2_score])
+
+func _on_Goal_1_body_entered(area):
+	if area == $Ball:
+		p2_score += 1
+		_print_score()
+		if p2_score >= game_score:
+			print("P2 Wins!")
+			$Ball.free()
+		else:
+			$Ball.reset()
+
+
+func _on_Goal_2_body_entered(area):
+	if area == $Ball:
+		p1_score += 1
+		_print_score()
+		if p1_score >= game_score:
+			print("P1 Wins!")
+			$Ball.free()
+		else:
+			$Ball.reset()
