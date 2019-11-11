ac12d83be524afdb368ac84ce86cbc7ebd5ed54f
Christopher Ray Langford <chris@hellocld.com>
2019-11-11 06:13:33 -0500

    Updated textures, background, window bounds

--- Ball.gd
+++ Ball.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 export var _speed = 10
 var _dir = Vector2(1, 1)
+var _radius = 0
+
 
 func _ready():
 	reset()
@@ -12,12 +14,14 @@ func _physics_process(delta):
 	if collision:
 		print(collision.collider.name)
 		_dir = _dir.bounce(collision.normal)
-	if position.x >= get_viewport().size.x or position.x < 0:
+	if position.x >= get_viewport().size.x - _radius or position.x < _radius:
 		_dir.x *= -1
-	if position.y >= get_viewport().size.y or position.y < 0:
+	if position.y >= get_viewport().size.y - _radius or position.y < _radius:
 		_dir.y *= -1
 
 
 func reset():
 	_dir = _dir.normalized()
 	position = get_viewport().size / 2
+	var shape = $CollisionShape2D.shape as CircleShape2D
+	_radius = shape.radius
