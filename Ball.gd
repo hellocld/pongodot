583c362084ad93f980107cb1492309f0d174a4d3
Christopher Ray Langford <chris@hellocld.com>
2019-11-14 14:34:41 -0500

    Sound effects, random ball direction, ball spawning

--- Ball.gd
+++ Ball.gd
@@ -5,28 +5,35 @@ var _dir = Vector2(1, 1)
 var _radius = 0
 var _hits = 0
 
-func _ready():
-	reset()
-
-
 func _physics_process(delta):
 	var collision = move_and_collide(_dir * _speed * delta)
 	if collision:
+		$SFX.play()
 		_hits += 1
-		if _hits % 10 == 0:
-			_speed += 2
+		if _hits % 3 == 0:
+			_speed += 10
 		print(collision.collider.name)
 		$AnimationPlayer.play("Boop")
 		_dir = _dir.bounce(collision.normal)
 	if position.x >= get_viewport().size.x - _radius or position.x < _radius:
+		$SFX.play()
 		_dir.x *= -1
 	if position.y >= get_viewport().size.y - _radius or position.y < _radius:
+		$SFX.play()
 		_dir.y *= -1
 
 
-func reset():
+func _ready():
+	randomize()
+	if(randf() > 0.5):
+		_dir.x = 1
+	else:
+		_dir.x = -1
+	if(randf() > 0.5):
+		_dir.y = 1
+	else:
+		_dir.y = -1
 	_dir = _dir.normalized()
-	position = get_viewport().size / 2
 	var shape = $CollisionShape2D.shape as CircleShape2D
 	_radius = shape.radius
 	_hits = 0
--- Game.gd
+++ Game.gd
@@ -5,24 +5,28 @@ export var game_score = 10
 var p1_score = 0
 var p2_score = 0
 
+var ball = preload("res://Ball.tscn")
 
 func _ready():
 	p1_score = 0
 	p2_score = 0
+	_reset()
 
 func _reset():
 	$LabelPanel/Label.text = "GET READY"
 	$LabelPanel.visible = true
 	$Timer.connect("timeout", self, "_start_game")
 	$Timer.one_shot = true
-	$Timer.start(5)
+	$Timer.start(3)
 
 
 func _start_game():
+	print("Game start!")
 	$LabelPanel.visible = false
-	
-	$Ball.reset()
-	
+	var t_ball = ball.instance()
+	t_ball.position = get_viewport_rect().size / 2
+	add_child(t_ball)
+
 
 func _print_score():
 	$HUD/Score.text = "%02d | %02d" % [p1_score, p2_score]
@@ -31,21 +35,23 @@ func _print_score():
 
 func _on_Goal_1_body_entered(area):
 	if area == $Ball:
+		$GoalSFX.play()
+		$Ball.free()
 		p2_score += 1
 		_print_score()
 		if p2_score >= game_score:
 			print("P2 Wins!")
-			$Ball.free()
 		else:
-			$Ball.reset()
+			_reset()
 
 
 func _on_Goal_2_body_entered(area):
 	if area == $Ball:
+		$GoalSFX.play()
+		$Ball.free()
 		p1_score += 1
 		_print_score()
 		if p1_score >= game_score:
 			print("P1 Wins!")
-			$Ball.free()
 		else:
-			$Ball.reset()
+			_reset()
